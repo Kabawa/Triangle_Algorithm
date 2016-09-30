cdfcf150c69d1aee2ac732b888a6ca31e536e70d
Kabawa <deymosmile@ukr.net>
2016-09-30 13:52:58 +0300

    add proj

---
+++ triangle algorithm/triangle algorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangle_algorithm
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Name of files: triangle_1.txt and triangle_2.txt \nPlease write name of file");
+            string filePath = Console.ReadLine();
+            while (!File.Exists(filePath))
+            {
+                Console.WriteLine("Invalid name of file. Please try again!");
+                filePath = Console.ReadLine();
+            }
+      
+            Triangle tr = new Triangle();
+            tr.ReadFile(filePath);
+
+            tr.FindMaxPath(tr.Elements);
+
+            Console.WriteLine("\nthe maximum path is " + tr.Elements[0,0]);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ triangle algorithm/triangle algorithm/Triangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangle_algorithm
+{
+    public class Triangle
+    {
+        public int[,] Elements { get; set; }
+
+        public void ReadFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            int size = lines.Length;
+            int[,] triangle = new int[size, size];
+            try
+            {
+                string line;
+                int lenght = 1;
+                int j = 0;
+
+                while (j < size)
+                {
+                    line = lines[j];
+                    string[] massLine = line.Split(new char[] {' '});
+
+
+                    for (int i = 0; i < lenght; i++)
+                    {
+                        triangle[j, i] = Int32.Parse(massLine[i]);
+                    }
+                    lenght++;
+                    j++;
+                }
+                Elements = triangle;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+        public void FindMaxPath(int[,] triangle)
+        {
+            int size = Convert.ToInt32(Math.Sqrt(Elements.Length));
+            for (int i = size - 1; i > 0; i--)
+            {
+                for (int j = 0; j < size - 1; j++)
+                {
+                    if (triangle[i, j] > triangle[i, j + 1])
+                    {
+                        triangle[i - 1, j] += triangle[i, j];
+                    }
+                    else
+                    {
+                        triangle[i - 1, j] += triangle[i, j + 1];
+                    }
+                }
+                size--;
+            }
+        }
+    }
+}
